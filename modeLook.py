67a4f893c2dd8ed055ebededd973cd13927f6196
Modun17 <2573450629@qq.com>
2021-03-05 14:00:11 +0800

    1

--- modeLook.py
+++ modeLook.py
@@ -1,6 +1,6 @@
 from DownMovieGui import Ui_MainWindow
 import sys
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import QFont, QPalette
